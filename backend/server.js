57a72de694c1a85f5b65d0fb21ffbb7b13146d14
Chad Hinds <chadrickj8@gmail.com>
2020-11-13 13:51:30 -0500

    Retool

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
-const { request } = require('http')
-
+const express = require('express')
 require('dotenv').config()
-const request = require('request')
 
+
+// Initiate express
+const app = express();
+app.use(express.json())
+
+// Get the Oath
+
+
+app.listen(3000, console.log('server running on 3000!', process.env.GET_TOKEN))
